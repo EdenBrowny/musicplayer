652e1f8d93faed9d57d29151b885628b3b53c160
orisa07037616291 <orisakwepeter@gmail.com>
2022-12-25 20:47:16 +0100

    first Commit

---
+++ src/Components/Player/Player.js
@@ -0,0 +1,122 @@
+import React from "react";
+import { useEffect, useState } from "react";
+import Koala from "../../assets/Koala.jpg";
+import useSound from "use-sound";
+import Spirit from "../../assets/Spirit.mp3";
+import { AiFillPlayCircle, AiFillPauseCircle } from "react-icons/ai";
+import { BiSkipNext, BiSkipPrevious } from "react-icons/bi";
+import { IconContext } from "react-icons";
+import "./Player.css";
+import { clear } from "@testing-library/user-event/dist/clear";
+
+export default function Player() {
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [time, setTime] = useState({
+    min: "",
+    sec: "",
+  });
+  const [currTime, setCurrTime] = useState({
+    min: "",
+    sec: "",
+  });
+
+  const [seconds, setSeconds] = useState();
+  const [Playing, { pause, duration, sound }] = useState(Spirit);
+
+  useEffect(() => {
+    if (duration) {
+      const sec = duration / 1000;
+      const min = Math.floor(sec / 60);
+      const secRemain = Math.floor(sec % 60);
+      setTime({
+        min: min,
+        sec: secRemain,
+      });
+    }
+  }, [isPlaying]);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (sound) {
+        setSeconds(sound.seek([]));
+        const min = Math.floor(sound.seek([]) / 60);
+        const sec = Math.floor(sound.seek([]) % 60);
+        setCurrTime({
+          min,
+          sec,
+        });
+      }
+    }, 1000);
+    return () => clearInterval(interval);
+  }, [sound]);
+
+  const playingButton = () => {
+    if (isPlaying) {
+      // pause();
+      setIsPlaying(false);
+    } else {
+      // Playing();
+      setIsPlaying(true);
+      console.log(Playing);
+    }
+  };
+
+  return (
+    <div className="component">
+      <h2>Playing Now</h2>
+      <img className="musicCover" src={Koala} />
+      <div>
+        <h3 className="title">Spirit</h3>
+        <p className="subTitle">Theo</p>
+      </div>
+
+      <div>
+        <div className="time">
+          <p>
+            {currTime.min}:{currTime.sec}
+          </p>
+          <p>
+            {time.min}:{time.sec}
+          </p>
+        </div>
+        <input
+          type="range"
+          min="0"
+          // max={duration / 1000}
+          default="0"
+          value={seconds}
+          className="timeline"
+          onChange={(e) => {
+            // sound.seek([e.target.value]);
+          }}
+        />
+      </div>
+      <div>
+        <button className="playButton">
+          <IconContext.Provider value={{ size: "3em", color: "#27AE60" }}>
+            <BiSkipPrevious />
+          </IconContext.Provider>
+        </button>
+        {!isPlaying ? (
+          <button className="playButton" onClick={playingButton}>
+            <IconContext.Provider value={{ size: "3em", color: "#27AE60" }}>
+              <AiFillPlayCircle />
+            </IconContext.Provider>
+          </button>
+        ) : (
+          <button className="playButton" onClick={playingButton}>
+            <IconContext.Provider value={{ size: "3em", color: "#27AE60" }}>
+              <AiFillPauseCircle />
+            </IconContext.Provider>
+          </button>
+        )}
+
+        <button className="playButton">
+          <IconContext.Provider value={{ size: "3em", color: "#27AE60" }}>
+            <BiSkipNext />
+          </IconContext.Provider>
+        </button>
+      </div>
+    </div>
+  );
+}
